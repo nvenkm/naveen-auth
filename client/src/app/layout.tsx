aab64c877403ebc44896bfe7d81de1652f2ff5be
naveen kumar <7naveennn@gmail.com>
2024-06-11 10:43:06 +0530

    Register done, toast added

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,27 +3,29 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import RecoilRootProvider from "@/providers/RecoilRootProvider";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Naveen Auth",
-    description: "",
+  title: "Naveen Auth",
+  description: "",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                <RecoilRootProvider>
-                    <Navbar />
-                    {children}
-                </RecoilRootProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <RecoilRootProvider>
+          <Toaster />
+          <Navbar />
+          {children}
+        </RecoilRootProvider>
+      </body>
+    </html>
+  );
 }
--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -6,147 +6,170 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
-const formSchema = z.object({
+const formSchema = z
+  .object({
     fullName: z.string().min(2, {
-        message: "Full name must be at least 2 characters.",
+      message: "Full name must be at least 2 characters.",
     }),
     email: z.string().email({
-        message: "Must be a valid email address.",
+      message: "Must be a valid email address.",
     }),
     password: z.string().min(6, {
-        message: "Password must be at least 6 characters.",
+      message: "Password must be at least 6 characters.",
     }),
     confirmPassword: z.string().min(6, {
-        message: "Password must be at least 6 characters.",
+      message: "Password must be at least 6 characters.",
     }),
-});
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match.",
+  });
 
 const RegisterPage = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            fullName: "",
-            email: "",
-            password: "",
-        },
-    });
+  const router = useRouter();
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values);
-    }
-    return (
-        <div className="flex flex-col items-center">
-            <div className="mt-6">
-                <h1 className=" text-4xl font-bold text-center">Register</h1>
-                <p className="text-center my-4">
-                    To get started, you need to signup here.
-                </p>
-            </div>
-            <div>
-                <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className="space-y-6 min-w-96 bg-white p-5 rounded-md border shadow-sm"
-                    >
-                        <FormField
-                            control={form.control}
-                            name="fullName"
-                            render={({ field }) => (
-                                <>
-                                    <FormItem>
-                                        <FormLabel>Full Name</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                className="bg-white"
-                                                placeholder="Full Name"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                </>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="email"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Email</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            className="bg-white"
-                                            placeholder="abcd@efg.com"
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        ></FormField>
-                        <FormField
-                            control={form.control}
-                            name="password"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Password</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            type="password"
-                                            className="bg-white"
-                                            placeholder="Password..."
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        ></FormField>
-                        <FormField
-                            control={form.control}
-                            name="confirmPassword"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Password</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            className="bg-white"
-                                            type="password"
-                                            placeholder="Confirm Password..."
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        ></FormField>
-                        <Button type="submit">Submit</Button>
-                    </form>
-                </Form>
-            </div>
-            <div className="mt-3 flex gap-2 ">
-                <span>Already have an account?</span>
-                <span>
-                    <Link className="text-primary" href="/login">
-                        Login
-                    </Link>
-                </span>
-            </div>
-        </div>
-    );
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    try {
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/register`,
+        values
+      );
+
+      if (res.data.success) {
+        toast.success(res.data.message);
+        form.reset();
+      }
+
+      console.log(res.data.success);
+    } catch (error) {}
+  }
+  return (
+    <div className="flex flex-col items-center">
+      <div className="mt-6">
+        <h1 className=" text-4xl font-bold text-center">Register</h1>
+        <p className="text-center my-4">
+          To get started, you need to signup here.
+        </p>
+      </div>
+      <div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-6 min-w-96 bg-white p-5 rounded-md border shadow-sm"
+          >
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <>
+                  <FormItem>
+                    <FormLabel>Full Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="bg-white"
+                        placeholder="Full Name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                </>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      placeholder="abcd@efg.com"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      className="bg-white"
+                      placeholder="Password..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      type="password"
+                      placeholder="Confirm Password..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+      <div className="mt-3 flex gap-2 ">
+        <span>Already have an account?</span>
+        <span>
+          <Link className="text-primary" href="/login">
+            Login
+          </Link>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
